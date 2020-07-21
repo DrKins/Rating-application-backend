249f020662c31331b5f5c5929e2d82411933c545
rmvc-mirza <ramovic.mirzaa@gmail.com>
2020-07-21 09:41:10 +0200

    Channging the database to the dockers local db

--- src/db/db.js
+++ src/db/db.js
@@ -1,25 +1,20 @@
 /**
  * Load SEQUELIZE
  */
+const config = require('../../config.js');
 const Sequelize = require('sequelize');
 
 /**
  * Load connection obj
  */
-const sequelize = new Sequelize(
-  'xuZXyWb00y',
-  // 'test', // Use when testing failed connection
-  'xuZXyWb00y',
-  'DQkiFTxMgL',
-  {
-    host: 'remotemysql.com',
+const sequelize = new Sequelize(config.db_name, config.db_user, config.db_password, {
+    host: config.db_host,
     dialect: 'mysql',
     logging: false,
     define: {
-      timestamps: false,
-      freezeTableName: true,
-    },
-  }
-);
+        timestamps: false,
+        freezeTableName: true
+    }
+});
 
 module.exports = sequelize;
