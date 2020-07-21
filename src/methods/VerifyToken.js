3891e270aa8865e984cafa7f1dd644f39672c438
rmvc-mirza <ramovic.mirzaa@gmail.com>
2020-07-21 11:52:32 +0200

    Added some comments

--- src/methods/VerifyToken.js
+++ src/methods/VerifyToken.js
@@ -1,12 +1,10 @@
-//      Loading dependencies
-
 const verification = {
     ver(req, res, next) {
-        const bearerHeader = req.headers['authorization'];
+        const bearerHeader = req.headers['authorization']; //takes the auth header
         if (typeof bearerHeader !== 'undefined') {
-            const bearer = bearerHeader.split(' ');
+            const bearer = bearerHeader.split(' '); // splitis the auth header at whitespace
             const bearerToken = bearer[1];
-            req.token = bearerToken;
+            req.token = bearerToken; //sets the req.token of next to the second part of the split auth token
             next();
         } else {
             res.sendStatus(403);
