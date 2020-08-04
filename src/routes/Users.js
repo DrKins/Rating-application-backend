6b8d809fe7e55fab66c832c58175614491dd15bd
rmvc-mirza <ramovic.mirzaa@gmail.com>
2020-08-04 12:09:27 +0200

    made changes to getting all users endpoint Now level 2 admin can request its companys users

--- src/routes/Users.js
+++ src/routes/Users.js
@@ -12,6 +12,7 @@ const Settings = require('../models/Settings');
 
 const verification = require('../methods/VerifyToken');
 
+
 const router = express.Router()
 
 router.post('/register', verification.ver, async (req, res) => {
@@ -27,45 +28,45 @@ router.post('/register', verification.ver, async (req, res) => {
                     }
                 }).then(company => {
                     if (!company) {
-Settings.create(
-    {message: "Hvala vam na glasanju", 
-    messageDuration: 5,
-    emoticonCount: 3, 
-    company: req.body.company,
-    emoticonPack: "yellowPack"});
+                        Settings.create({
+                            message: "Hvala vam na glasanju",
+                            messageDuration: 5,
+                            emoticonCount: 3,
+                            company: req.body.company,
+                            emoticonPack: "yellowPack"
+                        });
                         console.log("Company Created");
-                         }
-                        User.findOne({
-                            where:{
-                                name: req.body.name
-                            }
-                        }).then(async (user) => {
-                            if (!user) {
-                                const HashedPW = await bcrypt.hash(req.body.password, 10);
-                                User.create({name: req.body.name, password: HashedPW, lvl: req.body.lvl, company: req.body.company});
-                                console.log("User created");
-                            } else {
-                                console.log("User exist");
-                            }
-                        }).catch(err => console.log(err))
-                        res.status(400).end();
-                    
+                    }
+                    User.findOne({
+                        where: {
+                            name: req.body.name
+                        }
+                    }).then(async (user) => {
+                        if (!user) {
+                            const HashedPW = await bcrypt.hash(req.body.password, 10);
+                            User.create({name: req.body.name, password: HashedPW, lvl: req.body.lvl, company: req.body.company});
+                            console.log("User created");
+                        } else {
+                            console.log("User exist");
+                        }
+                    }).catch(err => console.log(err))
+                    res.status(400).end();
+
                 }).catch(err => console.log(err))
             } else 
                 res.sendStatus(403);
             
+
         }
     })
 });
-
-
+// Route used for user login 
 router.post('/login', (req, res) => {
     User.findOne({
             where: {
                 name: req.body.username
             }
         }).then(async response => {
-
             try {
                     await bcrypt.compare(req.body.password, response.dataValues.password, (err, succes) => {
                         if (err) {
@@ -88,28 +89,40 @@ router.post('/login', (req, res) => {
             console.log(err);
         })}
 );
-
+// Route used to list all users if admin 3 all user in general if lvl 2 users from his company
 router.get('/getallusers', verification.ver, (req, res) => {
     jwt.verify(req.token, config.privkey, (err, AuthData) => {
         if (err) {
             res.sendStatus(403);
         } else {
-            if (AuthData.lvl > 2) {
-                User.findAll({raw : true})
-                    .then(users => {
-                        users.forEach(x=>
-                            {
-                              delete x.password
-                            })
+            if (AuthData.lvl == 3) {
+                User.findAll({raw: true}).then(users => {
+                    users.forEach(user => {
+                        delete user.password
+                    })
                     console.log("ALL USERS Got");
                     res.status(200).end(JSON.stringify(users));
                 }).catch(err => console.log(err))
-            } else 
+            } else if (AuthData.lvl == 2) {
+                User.findAll({
+                    where: {
+                        company: AuthData.company
+                    }
+                }).then(users => {
+                    users.forEach(user => {
+                        delete user.password
+                    })
+                    res.status(200).end(JSON.stringify(users));
+                    console.log("All users sent");
+                }).catch(err => {
+                    console.log(err);
+                    res.sendStatus(500);
+                })
+            } else {
                 res.sendStatus(403);
-        
+            }
         }
     })
 });
 
 module.exports = router;
-
